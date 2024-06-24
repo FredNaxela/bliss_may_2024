558c765de2f4f3c35103b97be6329c125a082100
Alexander Fedyushin <fedyushinap@gmail.com>
2024-06-24 16:20:56 +0300

    change migrate

--- bliss_may_2024/settings.py
+++ bliss_may_2024/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 DATABASES_URL = os.environ.get('DATABASES_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['os.environ.get("ALLOWED_HOSTS")', '*']
 
